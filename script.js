14c7917f3c551d2af1615949eb3a1732de50727e
Rachna Ahlawat <rachnaahlawat76@gmail.com>
2026-06-19 17:30:31 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function downloadResume() {
-    alert("Resume download feature coming soon!");
-}
-
-document.querySelector("form").addEventListener("submit", function(e){
-    e.preventDefault();
-    alert("Message Sent Successfully!");
-});
